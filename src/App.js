0fe38d0947cab0213f84baed7aa75ef317109b51
luis armendariz <luisarmendariz@luiss-MacBook-Pro.local>
2021-05-20 10:54:41 -0600

    Add crud routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
 import './App.css';
+import { Route, Link, Switch } from "react-router-dom";
 
 function App() {
+// URL variable to hold backend url 
+const url = "https://tunr-backend-cd.herokuapp.com"
+
+
+// initial state to hold the list of songs
+const [songs, setSongs] = React.useState([])
+
+// create empty song variable 
+const emptySong = {
+  title: "",
+  artist: "",
+  time: 0
+}
+
+const [selectSong, setSelectSong] = React.useState(emptySong)
+
+// function to get list of songs
+const getSongs = () => {
+  fetch(url + "/tune/")
+  .then((response) => response.json())
+  .then((data) => 
+  setSongs(data))
+}
+
+// useEffect acts as a page onload
+React.useEffect(() => {
+  getSongs()
+},[])
+
+// handleCreate/"POST"/  creates a new song 
+const handleCreate = (newSong) => {
+  fetch(url + "/tune/", {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(newSong)
+  })
+  .then(() => getSongs())
+}
+
+// handleUpdate/"PUT"/updates the song 
+const handleUpdate = (updateSong) => {
+  fetch(url + "/tune/" + updateSong._id,{
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    .then(() => getSongs())
+  })
+}
+
+// function to specifiy which song we are updating the state with
+const selectSong = (song) => {
+  setSelectSong(song)
+}
+
+
+const deleteSong = (song) => {
+  fetch(url + "/tune/" + song._id, {
+    method: "delete"
+  })
+  .then(() => getSongs())
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TUNR.</h1>
+      <h3>FOR ALL YOUR PLAYLIST NEEDS</h3>
+      <hr />
     </div>
   );
 }
